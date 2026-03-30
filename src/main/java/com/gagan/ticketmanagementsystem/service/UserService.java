798a06dc82fc55e550da81c7e260687dd08abc5c
gagan <gaganjio14@gmail.com>
2026-03-30 17:22:31 +0530

    Add UserService with basic CRUD operations

---
+++ src/main/java/com/gagan/ticketmanagementsystem/service/UserService.java
@@ -0,0 +1,23 @@
+package com.gagan.ticketmanagementsystem.service;
+
+import com.gagan.ticketmanagementsystem.entity.User;
+import com.gagan.ticketmanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
