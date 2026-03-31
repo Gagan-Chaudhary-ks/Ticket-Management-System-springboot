4fe40463d513316f33183dee3b14416b27a5fb87
gagan <gaganjio14@gmail.com>
2026-03-31 16:56:22 +0530

    feat: add TicketRepository interface with JpaRepository support

--- src/main/java/com/gagan/ticketmanagementsystem/repository/TicketRepository.java
+++ src/main/java/com/gagan/ticketmanagementsystem/repository/TicketRepository.java
@@ -1,9 +1,9 @@
 package com.gagan.ticketmanagementsystem.repository;
 
-import com.gagan.ticketmanagementsystem.entity.User;
+import com.gagan.ticketmanagementsystem.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface TicketRepository extends JpaRepository <Ticket,Integer> {
 }
