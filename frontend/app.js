a587225705469852bdd953db6207f007f90acc5f
gagan <gaganjio14@gmail.com>
2026-05-21 17:26:35 +0530

    feat: implement fetch API request to read tickets from backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,22 @@
+const API_URL = "http://localhost:8080/api/tickets";
+
 const loadButton = document.getElementById("loadticketsbtn");
 
-function handleButtonClick() {
-    alert("Connection established! JavaScript is listening to your button click.");
+async function fetchTickets() {
+    try{
+        const response = await fetch(API_URL);
+
+        if(!response.ok){
+            throw new error(`server returned a bad response ${response.status}`);
+        }
+
+        const tickets = await response.json();
+
+        console.log("success! Data recieved from database: " , tickets);
+
+    }catch(error){
+        console.error("Network communication failure !", error);
+    }
 }
 
-loadButton.addEventListener("click", handleButtonClick);
+loadButton.addEventListener("click", fetchTickets);
